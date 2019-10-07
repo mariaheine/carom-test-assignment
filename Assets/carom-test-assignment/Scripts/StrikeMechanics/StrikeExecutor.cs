d7d6ada736d409cf7807f02992ddeb0ce9d31fa5
Maria Heine <altanhir@gmail.com>
2019-10-07 17:07:58 +0200

    Replay button visibility fix

--- Assets/carom-test-assignment/Scripts/StrikeMechanics/StrikeExecutor.cs
+++ Assets/carom-test-assignment/Scripts/StrikeMechanics/StrikeExecutor.cs
@@ -1,63 +1,58 @@
 ﻿using UnityEngine;
 
-public class StrikeExecutor : MonoBehaviour
-{   
+public class StrikeExecutor : MonoBehaviour {
     [SerializeField] CameraController cameraController;
     [SerializeField] CreateStrike createStrike;
     [SerializeField] StrikeRenderer strikeRenderer;
     [SerializeField] GameObject ReplayButton;
+    [SerializeField] GameStateManager gameStateManager;
 
     static StrikeExecutor instance;
     StrikeResolver strikeResolver;
     Strike lastStrike;
 
-    void Awake()
-    {
+    void Awake () {
         instance = this;
-        strikeResolver = GetComponent<StrikeResolver>();
-        ReplayButton.SetActive(false);
+        strikeResolver = GetComponent<StrikeResolver> ();
     }
 
-    void Start()
-    {
-        createStrike.onStrike += strike => Execute(strike);
+    void Start () {
+        createStrike.onStrike += strike => Execute (strike);
         strikeResolver.onResolved += ReturnToGame;
+        gameStateManager.onGameStarted += () => { ReplayButton.SetActive (false); };
     }
 
-    public void Execute(Strike strike)
-    {
-        Debug.Log("Execute", transform);
+    public void Execute (Strike strike) {
+        Debug.Log ("Execute", transform);
 
         lastStrike = strike;
 
-        cameraController.RequestCamera(CameraController.CameraType.Watch);
+        cameraController.RequestCamera (CameraController.CameraType.Watch);
 
         createStrike.enabled = false;
         strikeRenderer.enabled = false;
-        ReplayButton.SetActive(false);
+        ReplayButton.SetActive (false);
 
-        strikeResolver.Resolve(StrikeResolver.ResolverType.Strike, lastStrike);
+        strikeResolver.Resolve (StrikeResolver.ResolverType.Strike, lastStrike);
     }
 
-    public void Replay()
-    {
-        Debug.Log("Replay", transform);
+    public void Replay () {
+        Debug.Log ("Replay", transform);
 
-        cameraController.RequestCamera(CameraController.CameraType.Watch);
+        cameraController.RequestCamera (CameraController.CameraType.Watch);
         createStrike.enabled = false;
         strikeRenderer.enabled = false;
-        ReplayButton.SetActive(false);
+        ReplayButton.SetActive (false);
 
-        strikeResolver.Resolve(StrikeResolver.ResolverType.Replay, lastStrike);     
+        strikeResolver.Resolve (StrikeResolver.ResolverType.Replay, lastStrike);
     }
 
-    void ReturnToGame(StrikeResolver.ResolverType resolverType)
-    {
-        Debug.Log("Resolved", transform);
-        
-        cameraController.RequestCamera(CameraController.CameraType.Play);
+    void ReturnToGame (StrikeResolver.ResolverType resolverType) {
+        Debug.Log ("Resolved", transform);
+
+        cameraController.RequestCamera (CameraController.CameraType.Play);
         createStrike.enabled = true;
         strikeRenderer.enabled = true;
-        ReplayButton.SetActive(true);
+        ReplayButton.SetActive (true);
     }
 }
